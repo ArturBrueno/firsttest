c748c63abd4be1677583fb163a4754e26f1131a9
ArturBrueno <49720126+ArturBrueno@users.noreply.github.com>
2020-05-05 10:58:09 +0300

    fist commit of 3 lesson
    05/05/2020

---
+++ page_objects/catalog/fragments/title_block.fragment.js
@@ -0,0 +1,9 @@
+class TitleBlockFragment {
+    constructor () {
+        this.root = $('main')
+        this.title  = this.root.$('.catalog-heading')
+    }
+}
+module.exports = {
+    TitleBlockFragment
+}
---
+++ specs/first.spec.js
@@ -0,0 +1,24 @@
+const expect = require('chai').expect
+const EC = protractor.ExpectedConditions
+
+const { MainPage, CatalogPage } = require('../page_objects')
+
+describe('First suit', function () {
+
+ 
+  const maniPage = new MainPage()
+  const catalogPage = new CatalogPage()   
+
+
+  it('first test', async function () {
+    const searchValue = 'телефон samsung'
+    
+    await browser.get('https://rozetka.com.ua/')
+    await maniPage.searchItems(searchValue)
+    const catalogTitleValue = await catalogPage.getTitleBlockInfo()
+
+    expect(catalogTitleValue.toLowerCase()).to.include('samsung')
+  })
+})
+
+
